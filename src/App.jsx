1b60f8df51cb89970de9c7bf4326d72ea22455fd
vedanth <vedanth.ramanathan@gmail.com>
2023-06-13 16:18:27 -0500

    updated SEO tools using Helmet-Async

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import {
   About,
   Contact,
@@ -12,22 +13,53 @@ import {
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
+    <HelmetProvider>
+      <BrowserRouter>
+        <Helmet>
+          <title>Vedanth Ramanathan</title>
+          <meta
+            name="description"
+            content="Coding Portfolio, Projects, and Activities of Vedanth Ramanathan! Senior at LASA High School in Austin, Texas"
+          />
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://vedanthramanathan.com/" />
+          <meta property="og:title" content="Vedanth Ramanathan" />
+          <meta
+            property="og:description"
+            content="Coding Portfolio, Projects, and Activities of Vedanth Ramanathan! Senior at LASA High School in Austin, Texas"
+          />
+          <meta property="og:image" content="" />
+
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta
+            property="twitter:url"
+            content="https://vedanthramanathan.com/"
+          />
+          <meta property="twitter:title" content="Vedanth Ramanathan" />
+          <meta
+            property="twitter:description"
+            content="Coding Portfolio, Projects, and Activities of Vedanth Ramanathan! Senior at LASA High School in Austin, Texas"
+          />
+          <meta property="twitter:image" content="" />
+        </Helmet>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+
+          <Tech />
+          <Works />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
         </div>
-        <About />
-        
-        <Tech />
-        <Works />
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 };
 
