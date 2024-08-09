8873f799f4632c30a84cafd03b790e803629170c
VedanthR5 <ragasv3@gmail.com>
2024-08-09 10:44:54 -0500

    shreya edited last commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Rat</span>
+            Hi, I'm <span className="text-[#915EFF]">Vedanth</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I'm a senior at LASA High School in Austin, Texas{" "}
